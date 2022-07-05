35b624d4ca5f0a77b39b1526488b0f373579381b
Vipula S Anandapiya <v.s.anandapiya@gmail.com>
2022-07-05 11:34:52 +0530

    Welcome blade updated with AlpineJS sample code

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes();
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [
+    App\Http\Controllers\HomeController::class, 'index'
+    ])->name('home');
