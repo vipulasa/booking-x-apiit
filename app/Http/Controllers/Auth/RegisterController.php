41a0e1d4252864fc40305fa173f1dd19f8216c02
Vipula S Anandapiya <v.s.anandapiya@gmail.com>
2022-07-19 10:16:26 +0530

    User model moved to new auth folder and related controllers / config was updated

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
+use App\Models\Auth\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
